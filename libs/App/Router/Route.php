7f79d01483e1a2fccec1468f9d3c95d418344388
Harald Lapp <h.lapp@clipdealer.de>
2020-07-16 16:08:48 +0200

    partly rewrite, handling of middleware

--- libs/App/Router/Route.php
+++ libs/App/Router/Route.php
@@ -13,10 +13,14 @@ declare(strict_types=1);
 
 namespace Octris\App\Router;
 
+use Octris\App\Middleware\MiddlewareDispatcherTrait;
+use Octris\App\MiddlewareCollector;
+use Octris\App\MiddlewareDispatcher;
+use Octris\App\Request\RequestHandlerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class Route
+class Route implements RequestHandlerInterface
 {
     /**
      * @var string[]
@@ -39,9 +43,19 @@ class Route
     protected $controller;
 
     /**
-     * @var callable[]
+     * @var RouteCollector
      */
-    protected array $middleware;
+    protected RouteCollector $group;
+
+    /**
+     * @var MiddlewareDispatcher
+     */
+    protected MiddlewareDispatcher $middleware_dispatcher;
+
+    /**
+     * @var bool
+     */
+    protected bool $group_middleware_added = false;
 
     /**
      * Constructor.
@@ -51,13 +65,31 @@ class Route
      * @param   string          $pattern
      * @param   callable        $controller
      * @param   string          $identifier
+     * @param   RouteCollector  $group
      */
-    public function __construct(array $methods, string $name, string $pattern, callable $controller)
+    public function __construct(array $methods, string $name, string $pattern, callable $controller, RouteCollector $group)
     {
         $this->methods = $methods;
         $this->name = $name;
         $this->pattern = $pattern;
         $this->controller = $controller;
+        $this->group = $group;
+
+        $handler = new class($controller) implements RequestHandlerInterface {
+            private $controller;
+
+            public function __construct(callable $controller)
+            {
+                $this->controller = $controller;
+            }
+
+            public function handle(Request $request): Response
+            {
+                return ($this->controller)($request);
+            }
+        };
+
+        $this->middleware_dispatcher = new MiddlewareDispatcher($handler);
     }
 
     /**
@@ -91,20 +123,38 @@ class Route
     }
 
     /**
-     * Handle route.
+     * Add middleware.
+     *
+     * @param   mixed           $middleware
+     * @return  mixed
      */
-    public function handle(Request $request, Response $response): Response
+    public function addMiddleware(mixed $middleware): self
     {
+        $this->middleware_dispatcher->addMiddleware($middleware);
 
+        return $this;
     }
 
     /**
-     * Add middleware.
+     * Handle route.
      *
-     * @param   MiddlewareInterface|callable|string $middleware
+     * @param   Request         $request
+     * @return  Response
      */
-    public function addMiddleware(MiddlewareInterface|callable|string $middleware): self
+    public function handle(Request $request): Response
     {
-        $this->middleware_dispatcher->addMiddleware($middleware);
+        if (!$this->group_middleware_added)  {
+            $inner = $this->middleware_dispatcher;
+            $this->middleware_dispatcher = new MiddlewareDispatcher($inner);
+
+            foreach (array_reverse($this->group->getAllMiddleware()) as $middleware) {
+                $this->middleware_dispatcher->addMiddleware($middleware);
+            }
+
+            $this->group_middleware_added = true;
+        }
+
+
+        return $this->middleware_dispatcher->handle($request);
     }
 }
